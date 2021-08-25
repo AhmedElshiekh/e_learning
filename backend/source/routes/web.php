a8dd63cb1c60301788c4dc7aa6803fc68ac0c9a9
AhmedElshiekh <ahmedel.sh50@gmail.com>
2021-08-25 15:49:37 +0200

    admin route related by subdomin

--- backend/source/routes/web.php
+++ backend/source/routes/web.php
@@ -13,19 +13,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::domain(env('APP_ADMIN_URL'))->group(function ($router) {
 
-Route::group(['prefix' => 'admin'], function () {
-  Route::get('/login', 'AdminAuth\LoginController@showLoginForm')->name('login');
-  Route::post('/login', 'AdminAuth\LoginController@login');
-  Route::post('/logout', 'AdminAuth\LoginController@logout')->name('logout');
+    Route::get('/', function () {
+       return redirect(url('admin/home'));
+    });
+    Route::group(['prefix' => 'admin'], function () {
+    Route::get('/login', 'AdminAuth\LoginController@showLoginForm')->name('login');
+    Route::post('/login', 'AdminAuth\LoginController@login');
+    Route::post('/logout', 'AdminAuth\LoginController@logout')->name('logout');
 
-  Route::get('/register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
-  Route::post('/register', 'AdminAuth\RegisterController@register');
+    Route::get('/register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('/register', 'AdminAuth\RegisterController@register');
 
-  Route::post('/password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
-  Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
-  Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
-  Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+    Route::post('/password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+    Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
+    Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+    Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+    });
 });
 
 // Route::any('/',  function () {
